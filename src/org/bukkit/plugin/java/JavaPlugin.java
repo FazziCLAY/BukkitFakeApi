276cd878340674aaef88c7b88f95db89e4b08354
FazziCLAY <fazziclay@gmail.com>
2023-02-04 08:50:04 +0300

    init

---
+++ src/org/bukkit/plugin/java/JavaPlugin.java
@@ -0,0 +1,26 @@
+package org.bukkit.plugin.java;
+
+import java.util.logging.Logger;
+
+public abstract class JavaPlugin {
+
+    public void onLoad() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public void onDisable() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public void onEnable() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public String getName() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public Logger getLogger() {
+        throw new RuntimeException("Stub!");
+    }
+}
